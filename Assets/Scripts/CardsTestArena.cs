ac7774f45edee26111c40327308559de94687496
Cent-AA <abishevamirhankz@gmail.com>
2026-06-07 15:52:38 +0500

    Card start
    For now only health changes. (Temp icons health is heavy)

---
+++ Assets/Scripts/CardsTestArena.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using System.Security.Cryptography;
+
+public class CardsTestArena : MonoBehaviour
+{
+    //public GameObject lvlup;
+    [SerializeField] private GameObject lvlup;
+    [SerializeField] private Image card1;
+    [SerializeField] private Image card2;
+    [SerializeField] private Image card3;
+    [SerializeField] private TMP_Text BuffTarget;
+    public int who=1 ;
+    private int card1Index;
+    private int card2Index;
+    private int card3Index;
+    private HashSet<int> uniqueIndices = new HashSet<int>();
+    [SerializeField] private Sprite[] cardSprites;
+    private string[] Target =
+    {
+        "YOU SHOUDNT SEE THIS",
+        "PLAYER 1 BUFFS",
+        "TEAM BUFFS",
+        "PLAYER 2 BUFFS",
+        };
+        
+    public void TestCard()
+    {
+    //TEMPORARY NEEDS TO BE ON LVL UP BUT WE DONT HAVE XP OR ANYTHIN
+        uniqueIndices.Clear();
+
+        while (uniqueIndices.Count < 3)
+        {
+            int num = Random.Range(0, cardSprites.Length); 
+            uniqueIndices.Add(num); 
+        }
+        
+        int[] selected = new int[3];
+        uniqueIndices.CopyTo(selected);
+        card1Index = selected[0];
+        card2Index = selected[1];
+        card3Index = selected[2];
+        card1.sprite = cardSprites[card1Index];
+        card2.sprite = cardSprites[card2Index];
+        card3.sprite = cardSprites[card3Index];
+        //card1.sprite = cardSprites[selected[0]];
+        //card2.sprite = cardSprites[selected[1]];
+        //card3.sprite = cardSprites[selected[2]];
+        BuffTarget.text =Target[who];
+        foreach (int index in uniqueIndices)
+        {
+            //Debug.Log("Selected Index: " + index);
+        }
+            lvlup.SetActive(true);
+            Time.timeScale = 0f;
+            Debug.Log(who);
+        }
+    public void PressCard()
+    {
+    Time.timeScale = 1f;
+
+    who++;
+
+    if (who >= 4)
+    {
+        who = 1;
+        lvlup.SetActive(false);
+        return;
+    }
+
+    TestCard();
+    }
+    public void SelectCard1()
+    {
+    ApplyCard(card1Index);
+    }
+
+    public void SelectCard2()
+    {
+    ApplyCard(card2Index);
+    }
+
+    public void SelectCard3()
+    {
+    ApplyCard(card3Index);
+    }
+    private void ApplyCard(int cardID)
+    {
+    switch (cardID)
+    {
+        case 0:
+            Debug.Log("Increase damage");
+            break;
+
+        case 1:
+            Debug.Log("Increase health");
+                if (who == 1)
+                {
+                    //Debug.Log("Player1");
+                    Debug.Log( Registry.Players[0].GetComponent<PlayerController>().maxHealth);
+                    Debug.Log( Registry.Players[0].GetComponent<PlayerController>().currentHealth);
+                    //Debug.Log("Players in registry: " + Registry.Players.Count);
+                    Registry.Players[0].GetComponent<PlayerController>().maxHealth *= 1.75f;
+                    Registry.Players[0].GetComponent<PlayerController>().currentHealth *= 1.75f;
+                    Debug.Log( Registry.Players[0].GetComponent<PlayerController>().maxHealth);
+                    Debug.Log( Registry.Players[0].GetComponent<PlayerController>().currentHealth);
+                }
+                if (who == 2)
+                {
+                   // Debug.Log("Both");
+                    Registry.Players[0].GetComponent<PlayerController>().maxHealth *= 1.75f;
+                    Registry.Players[0].GetComponent<PlayerController>().currentHealth *= 1.75f;                   
+                    Registry.Players[1].GetComponent<PlayerController>().maxHealth *= 1.75f;
+                    Registry.Players[1].GetComponent<PlayerController>().currentHealth *= 1.75f;
+                }
+                if (who == 3)
+                {
+                    Registry.Players[1].GetComponent<PlayerController>().maxHealth *= 1.75f;
+                    Registry.Players[1].GetComponent<PlayerController>().currentHealth *= 1.75f;
+                }
+            break;
+
+        case 2:
+            Debug.Log("Increase speed");
+            break;
+        case 3:
+            Debug.Log("Increase STUFF");
+            break;
+        default:
+            Debug.Log("Something idk");
+            break;
+    }
+    PressCard();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Transform pl1 = Registry.Players[0];
+        //Transform pl2 = Registry.Players[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //NEED TO CHANGE SO ITS ON LVL UP / XP GAINED ON KILLS/ XP REQS INCREASES WITH EACH LVL.
+        if (Keyboard.current != null && Keyboard.current.oKey.wasPressedThisFrame)
+        {
+            TestCard();
+        }
+    }
+}
